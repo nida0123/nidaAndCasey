a718a28f863d16c1dabc316f2c26f67270de7781
nida0123 <nida.chacarpalubinskas@gmail.com>
2021-01-02 14:38:53 -0500

    commit1
    s

---
+++ javaScript.js
@@ -0,0 +1,21 @@
+function myFunction() {
+    var oDates= Array('virtual museum tour','go on a walk on facetime','movie date- nida picks', 'movie date- casey picks', 'continue a tv show or start a new one', 'personality quizzes','truth or worse truth','play a video game','make slideshow presentations and present to each other. ex: why frogs always look better with hats on');
+    var online = oDates[Math.floor(Math.random() * oDates.length)];
+    return online;
+}
+function myFunction2() {
+    var tDates= Array('get food- rock paper scissors for who decides the place','go for boba','on a picnic','movie date- nida picks', 'movie date- casey picks','do each others makeup','thrifting date');
+    var together = tDates[Math.floor(Math.random() * tDates.length)];
+    return together;
+  }
+/*.online {
+myFunction() {
+    var oDates= Array('virtual museum tour','go on a walk on facetime','movie date- nida picks', 'movie date- casey picks', 'continue a tv show or start a new one', 'personality quizzes','truth or worse truth','play a video game','make slideshow presentations and present to each other. ex: why frogs always look better with hats on');
+    var online = oDates[Math.floor(Math.random() * oDates.length)];
+}
+}
+.together {
+    myFunction2()
+    var tDates= Array('get food- rock paper scissors for who decides the place','go for boba','on a picnic','movie date- nida picks', 'movie date- casey picks','do each others makeup','thrifting date');
+    var together = tDates[Math.floor(Math.random() * tDates.length)];
+}*/
